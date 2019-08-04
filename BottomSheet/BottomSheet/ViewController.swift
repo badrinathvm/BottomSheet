20e714f7482360e6ec71fd0ec36a5738282c1c79
Badarinath Venkatnarayansetty <Badarinath_Venkatnarayansetty@intuit.com>
2019-08-03 22:24:38 -0700

    Implemented bottomshet notch

--- BottomSheet/BottomSheet/ViewController.swift
+++ BottomSheet/BottomSheet/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var containerView:UIView  = {
+        let view = UIView()
+        view.backgroundColor = UIColor.clear
+        return view
+    }()
+    
+    var notchView: UIView = {
+        let notchView = UIView()
+        notchView.backgroundColor = UIColor.blue
+        notchView.layer.cornerRadius = 4.0
+        return notchView
+    }()
+    
+    
     let button: UIButton = {
        let button = UIButton(type: UIButton.ButtonType.infoDark)
        button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self, action: #selector(showTransparent(_:)), for: UIControl.Event.touchUpInside)
+       button.addTarget(self, action: #selector(showTransparent(_:)), for: UIControl.Event.touchUpInside)
         return button
+    }()
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.yellow
+        return cv
     }()
 
     public fileprivate(set) var blackOverlay: UIControl = UIControl()
@@ -45,6 +66,19 @@ class ViewController: UIViewController {
         self.blackOverlay.frame = rootView.bounds
         self.blackOverlay.backgroundColor = UIColor(white: 0.0, alpha: 0.6)
         rootView.addSubview(self.blackOverlay)
+        rootView.addSubview(self.collectionView)
+        containerView.addSubview(notchView)
+        rootView.addSubview(containerView)
+        
+        let screenWidth = UIScreen.main.bounds
+        self.notchView.frame = CGRect(x: screenWidth.size.width / 3 + 40, y: 9, width: 50, height: 6)
+        self.containerView.frame = CGRect(x: 0, y: rootView.frame.height, width: rootView.frame.width, height: 24)
+        
+        let referenceHeight:CGFloat = 200
+        let y = rootView.frame.height - referenceHeight
+        self.collectionView.frame = CGRect(x: 0, y: rootView.frame.height, width: rootView.frame.width, height: referenceHeight)
+        
+        
         
         //create a blur effect
         let overlayBlur = UIBlurEffect(style: UIBlurEffect.Style.light)
@@ -53,12 +87,16 @@ class ViewController: UIViewController {
         effectView.isUserInteractionEnabled = false
         
         //add the blur effectuve view to blackoverlay
-        self.blackOverlay.addSubview(effectView)
+        //self.blackOverlay.addSubview(effectView)
         self.blackOverlay.alpha = 0
         
         //animate from alpha 0 to 1 to show the effect
         UIView.animate(withDuration: 0.5) {
             self.blackOverlay.alpha = 1
+            
+            self.collectionView.frame = CGRect(x: 0, y: y, width: self.collectionView.frame.width, height: self.collectionView.frame.height)
+            self.containerView.frame = CGRect(x: 0, y: y - 20, width: rootView.frame.width, height: 20)
+
         }
 
         //add a target to remove the overlay
@@ -67,6 +105,7 @@ class ViewController: UIViewController {
     
     @objc func removePopover() {
         self.blackOverlay.removeFromSuperview()
+        self.collectionView.removeFromSuperview()
     }
 }
 
